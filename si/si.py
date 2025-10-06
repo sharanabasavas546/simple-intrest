e9950f5ff28f758138a855a6dce1e48e7d601e8f
sharanabasavas546 <sharanabasavas546@gmail.com>
2025-10-06 16:14:40 +0530

    Update si.py

--- si/si.py
+++ si/si.py
@@ -3,5 +3,4 @@ r-float(input("Enter rate of interest:"))
 t-int(input("Enter time (in years):"))
 si-(p*t*r)/100
 print("Simple interest is:",si)
-ci=p*((1+r/100)**t)-p
-print("compund interest is :",ci)
+
